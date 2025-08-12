783407217488f04fef607beef4e29e6ecb41ba04
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-08-11 19:08:01 -0500

    feat: actualizar configuración de RainbowKit para usar Sepolia y cambiar el nombre de la aplicación a TARPUQKUNA

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
-import { anvil } from "wagmi/chains"
+import { sepolia } from "wagmi/chains"
+import { http } from "wagmi"
 
 export default getDefaultConfig({
-    appName: "NFT Marketplace",
+    appName: "TARPUQKUNA",
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [anvil],
+    chains: [sepolia],
+    transports: {
+        [sepolia.id]: http(process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL!)
+    },
     ssr: true,
 })
